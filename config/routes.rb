4b961e23087336114756a59635069aca115cb60b
Varsik Orchanian <varsenik.ava@gmail.com>
2024-09-20 16:19:04 +0200

    My Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  Rails.application.routes.draw do
+    get "up" => "rails/health#show", as: :rails_health_check
+
+    get 'tasks', to: 'tasks#index'
+
+    get 'tasks/new', to: 'tasks#new'
+    get 'tasks/:id', to: 'tasks#show'
+
+    post 'tasks', to: 'tasks#create'
+
+    get 'tasks/:id/edit', to: 'tasks#edit'
+    patch 'tasks/:id', to: 'tasks#update'
+
+    delete 'tasks/:id', to: 'tasks#destroy'
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
